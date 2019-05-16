cf463b3534bb48b4bb74a4ea5637a0ec26bf0061
amao1602 <1045648861@qq.com>
2019-05-16 09:06:48 +0800

    提交到本地仓库

---
+++ student_pro/src/com/student/model/StudentModel.java
@@ -0,0 +1,18 @@
+package com.student.model;
+
+public class StudentModel {
+	private int id;
+	private String name;
+	private int sex;
+	private int age;
+
+	@Override
+	public String toString() {
+		return "StudentModel [id=" + id + ", name=" + name + ", sex=" + sex + ", age=" + age + "]";
+	}
+
+	public StudentModel() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
